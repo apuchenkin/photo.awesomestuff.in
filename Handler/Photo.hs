9b6b15b2080ab4014b253035f6eeba2dc200f6f2
Artem Puchenkin <apuchenkin@gmail.com>
2015-04-06 19:59:58 +0300

    GET /photo

--- Handler/Photo.hs
+++ Handler/Photo.hs
@@ -1,10 +1,13 @@
 module Handler.Photo where
 
 import Import
-import System.Directory
 
-getPhotoR :: Handler Value
-getPhotoR = error "Not yet implemented: getPhotoR"
+getPhotoR :: PhotoId -> Handler Value
+getPhotoR photoId = do
+    photo <- runDB $ get photoId
+    let mViews = fmap succ (fmap photoViews photo)
+    runDB $ update photoId [PhotoViews  =. case mViews of {Nothing -> 0; Just views -> views}]
+    returnJson photo
 
 getPhotosR :: Handler Value
 getPhotosR = do
