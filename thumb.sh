0054e3b734b000e9b9657bc3e7c45d39efe98106
Artem Puchenkin <apuchenkin@gmail.com>
2015-04-24 16:12:54 +0300

    optimised images generator

--- thumb.sh
+++ thumb.sh
@@ -1,10 +1,19 @@
 #/bin/bash
 
-pushd static/gallery
+pushd static/src
+
 gm \
 mogrify \
 -output-directory ../thumb \
 -create-directories \
 -resize 320x200 \
 */*.jpg
+
+gm \
+mogrify \
+-output-directory ../gallery \
+-create-directories \
+-resize x1080 \
+*/*.jpg
+
 popd 
