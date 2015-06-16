231e1dd082a0209b915fdd358186a439aee9975f
Artem Puchenkin <apuchenkin@gmail.com>
2015-06-16 15:33:46 +0300

    CS Static handler

--- Handler/Static.hs
+++ Handler/Static.hs
@@ -19,85 +19,86 @@ import           System.FilePath.Posix  (takeExtension)
 
 remap :: Int -> Int -> Int -> Int -> (Int, Int)
 remap h w h' w'
-    | r > 1 = (h', w * h' `div` h)
-    | r < 1 = (w' * h `div` w, w')
-    | otherwise = (h', w')
-    where r = (w'* h) % (w * h')
+  | r > 1 = (h', w * h' `div` h)
+  | r < 1 = (w' * h `div` w, w')
+  | otherwise = (h', w')
+  where r = (w'* h) % (w * h')
 
 checkSign :: String -> String -> String -> Bool
 checkSign phrase secret sign = sign == sign'
-    where
-        digest  = hmac (fromString secret) $ fromString phrase :: HMAC SHA1
-        sign'   = toString $ digestToHexByteString $ hmacGetDigest digest
+  where
+    digest  = hmac (fromString secret) $ fromString phrase :: HMAC SHA1
+    sign'   = toString $ digestToHexByteString $ hmacGetDigest digest
 
 openCVresize :: String -> String -> Int -> Int -> IO ()
 openCVresize src dest w h = do
-    image   <- cvLoadImage src LoadUnchanged
-    nch     <- getNumChannels image
-    depth   <- getDepth image
-    image'  <- cvCreateImage (CvSize (fromIntegral w) (fromIntegral h)) nch depth
-    cvResize image image' CV_INTER_AREA
-    cvSaveImage dest image'
-    cvReleaseImage image
-    cvReleaseImage image'
+  image   <- cvLoadImage src LoadUnchanged
+  nch     <- getNumChannels image
+  depth   <- getDepth image
+  image'  <- cvCreateImage (CvSize (fromIntegral w) (fromIntegral h)) nch depth
+  cvResize image image' CV_INTER_AREA
+  cvSaveImage dest image'
+  cvReleaseImage image
+  cvReleaseImage image'
 
 getStaticPhotoR :: PhotoId -> Int -> Int -> String -> Handler TypedContent
 getStaticPhotoR pid w' h' sign = do
-    app <- getYesod
-    let settings = appSettings app
-        phrase   = show (fromSqlKey pid) ++ "-" ++ show w' ++ "x" ++ show h'
-    _ <- if checkSign phrase (secret settings) sign then return () else notFound
+  neverExpires
+  app <- getYesod
+  let settings = appSettings app
+      phrase   = show (fromSqlKey pid) ++ "-" ++ show w' ++ "x" ++ show h'
+  _ <- unless (checkSign phrase (secret settings) sign) notFound
 
-    mphoto <- runDB $ get pid
-    case mphoto of {Nothing -> notFound; Just photo -> processPhoto (cachePath settings) photo}
-    where
-        processPhoto :: String -> Photo -> Handler TypedContent
-        processPhoto cachePath photo = do
-            let (w, h)      = (photoWidth photo, photoHeight photo)
-                (h'', w'')  = remap h w h' w'
-                pth         = show (fromSqlKey pid) ++ "-" ++ show w'' ++ "x" ++ show h''
-                cacheFile   = cachePath ++ pth ++ takeExtension (photoSrc photo)
+  mphoto <- runDB $ get pid
+  case mphoto of {Nothing -> notFound; Just photo -> processPhoto (cachePath settings) photo}
+  where
+    processPhoto :: String -> Photo -> Handler TypedContent
+    processPhoto cachePath photo = do
+      let (w, h)      = (photoWidth photo, photoHeight photo)
+          (h'', w'')  = remap h w h' w'
+          pth         = show (fromSqlKey pid) ++ "-" ++ show w'' ++ "x" ++ show h''
+          cacheFile   = cachePath ++ pth ++ takeExtension (photoSrc photo)
 
-            neverExpires
-            if h'' > h || w'' > w
-              then sendFile typeJpeg $ photoSrc photo
-              else do
-              isCached    <- liftIO $ doesFileExist cacheFile
-              if isCached
-                then sendFile typeJpeg cacheFile
-                else do
-                  liftIO $ openCVresize (photoSrc photo) cacheFile w'' h''
-                  sendFile typeJpeg cacheFile
+      if h'' > h || w'' > w
+        then sendFile typeJpeg $ photoSrc photo
+        else do
+        isCached    <- liftIO $ doesFileExist cacheFile
+        if isCached
+          then sendFile typeJpeg cacheFile
+          else do
+            liftIO $ openCVresize (photoSrc photo) cacheFile w'' h''
+            sendFile typeJpeg cacheFile
 
 getStaticImageR :: String -> Int -> Int -> String -> Handler TypedContent
 getStaticImageR encpath w' h' s = processPhoto (B64.decodeLenient $ fromString encpath)
-    where
-        processPhoto :: ByteString -> Handler TypedContent
-        processPhoto bpath = do
-            app <- getYesod
+  where
+    processPhoto :: ByteString -> Handler TypedContent
+    processPhoto bpath = do
+      neverExpires
+      app <- getYesod
 
-            let settings = appSettings app
-                path    = toString bpath
-                pth     = encpath ++ "-" ++ show w' ++ "x" ++ show h'
-                digest  = hmac (fromString $ secret settings) $ fromString pth :: HMAC SHA1
-                sign = digestToHexByteString $ hmacGetDigest digest
-                cacheFile = cachePath settings ++ pth ++ takeExtension path
+      let settings = appSettings app
+          path    = toString bpath
+          pth     = encpath ++ "-" ++ show w' ++ "x" ++ show h'
+          digest  = hmac (fromString $ secret settings) $ fromString pth :: HMAC SHA1
+          sign = digestToHexByteString $ hmacGetDigest digest
+          cacheFile = cachePath settings ++ pth ++ takeExtension path
 
-            _           <- if sign == fromString s then return () else notFound
-            isCached    <- liftIO $ doesFileExist cacheFile
-            neverExpires
-            if isCached
-                then sendFile typeJpeg cacheFile
-                else saveCache path cacheFile
+      _           <- unless (sign == fromString s) notFound
+      isCached    <- liftIO $ doesFileExist cacheFile
 
-            where
-                saveCache :: String -> String -> Handler TypedContent
-                saveCache path cachePath = do
-                    image <- liftIO $ cvLoadImage path LoadUnchanged
-                    size  <- liftIO $ cvGetSize image
-                    let (w, h) = (sizeWidth size, sizeHeight size)
-                    let (h'', w'') = remap (fromIntegral h) (fromIntegral w) (fromIntegral h') (fromIntegral w')
-                    liftIO $ cvReleaseImage image
-                    liftIO $ openCVresize path cachePath w'' h''
+      if isCached
+        then sendFile typeJpeg cacheFile
+        else saveCache path cacheFile
 
-                    sendFile typeJpeg cachePath
+      where
+        saveCache :: String -> String -> Handler TypedContent
+        saveCache path cachePath = do
+          image <- liftIO $ cvLoadImage path LoadUnchanged
+          size  <- liftIO $ cvGetSize image
+          let (w, h) = (sizeWidth size, sizeHeight size)
+          let (h'', w'') = remap (fromIntegral h) (fromIntegral w) (fromIntegral h') (fromIntegral w')
+          liftIO $ cvReleaseImage image
+          liftIO $ openCVresize path cachePath w'' h''
+
+          sendFile typeJpeg cachePath
