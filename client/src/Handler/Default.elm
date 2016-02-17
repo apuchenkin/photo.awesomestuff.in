277378c90ac304d42612dcad889567b3b885a599
Artem Puchenkin <apuchenkin@gmail.com>
2016-02-18 00:06:34 +0300

    nav

--- client/src/Handler/Default.elm
+++ client/src/Handler/Default.elm
@@ -1,6 +1,7 @@
 module Handler.Default where
 
 import Dict
+import Either
 import Lib.Helpers exposing (noFx, singleton)
 import Handler.Actions exposing (..)
 import Lib.Types exposing (Router (..), Handler, Response (..))
@@ -75,17 +76,25 @@ categoryHandler router =
     view address state parsed =
       let
         -- _ = Debug.log "categoryHandler" state
-        category = getCategory state
+        -- category = getCategory state
+        category = Dict.get "category" state.router.params &> flip Dict.get state.categories
+        subcategory = Dict.get "subcategory" state.router.params &> flip Dict.get state.categories
 
         -- TODO: tuple is not equal by reference
-        categories c = Html.nav [class "categories"] <| List.map (\c -> categoryLink router (snd c) state.locale) <| Dict.toList c
+        navigation = Html.nav [class "categories"] [
+          Html.ul []
+            <| List.map (\c -> Html.li [] [categoryLink router (snd c) state.locale])
+            <| List.filter (\(_, Category pc) -> Maybe.withDefault False <| pc.parent &> \ p -> flip Maybe.map category (\(Category c) -> Either.elim ((==) (c.id)) ((==) (Category c)) p) )
+            <| Dict.toList state.categories
+        ]
+
         photos =  (\r params photos -> (\l -> div [] <| List.map (\p -> photoLink r p state.router.params) l) photos) router state.router.params state.photos
         parsed' = div [] <| Maybe.withDefault [] <| Maybe.map singleton parsed
 
         -- navigation = Maybe.map (\c -> Html.nav div [class "navigation"] [, ] ) category
       in flip Maybe.map category <| \c -> div [] [
-        text <| toString c,
-        categories state.categories,
+        -- text <| toString c,
+        navigation,
         photos, parsed'
       ]
   in
