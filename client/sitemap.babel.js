aeccc0a3d14d8f827b7a654ac4ad13b385fc0585
Puchenkin Artem <apuchenkin@gmail.com>
2017-07-13 23:55:01 +0300

    Minor updates

--- client/sitemap.babel.js
+++ client/sitemap.babel.js
@@ -1,13 +1,12 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import sitemap from 'sitemap';
-import { Promise } from 'es6-promise';
+// import { Promise } from 'es6-promise';
 
 import config from './src/etc/config.json';
-import CategoryService from './lib/service/Category';
-import PhotoService from './lib/service/Photo';
-import PageService from './lib/service/Page';
-import utils from './src/lib/utils';
-import { getSrc } from './lib/util/photo';
+import CategoryService from '../common/service/api/Category';
+import PhotoService from '../common/service/api/Photo';
+import PageService from '../common/service/api/Page';
+import { getSrc } from '../common/service/photo';
 
 const merge = urls => urls.reduce((acc, location) => {
   const exist = acc.find(a => a.url === location.url);
@@ -18,12 +17,19 @@ const merge = urls => urls.reduce((acc, location) => {
   return exist ? acc : acc.concat([location]);
 }, []);
 
+const fetchAll = (object) => {
+  const keys = Object.keys(object);
+
+  return Promise
+    .all(keys.map(k => object[k]))
+    .then(data => keys.reduce((acc, k, idx) => Object.assign(acc, { [k]: data[idx] }), {}));
+};
 
 const createLinks = urls => urls.map(url =>
   Object.assign(url, { links: [{ lang: 'x-default', url: url.url }].concat(
-      url.langs.map(lang => ({ lang, url: `/${lang}${url.url}` }))
+      url.langs.map(lang => ({ lang, url: `/${lang}${url.url}` })),
     ),
-   })
+  }),
 );
 
 const createSitemap = urls => sitemap.createSitemap({
@@ -35,21 +41,20 @@ const createSitemap = urls => sitemap.createSitemap({
 
 
 const pages = config.locales.map((locale) => {
-  const
-    defaults = { locale, apiEndpoint: config.apiEndpoint },
-    categoryService = new CategoryService(defaults),
-    photoService = new PhotoService(defaults),
-    pageService = new PageService(defaults);
+  const defaults = { locale, apiEndpoint: config.apiEndpoint };
+  const categoryService = new CategoryService(defaults);
+  const photoService = new PhotoService(defaults);
+  const pageService = new PageService(defaults);
 
-  return utils.fetchAll({
+  return fetchAll({
     locale: Promise.resolve(locale),
     pages: pageService.fetchPages(),
     categories: categoryService.fetchCategories().then(categories =>
       Promise.all(categories.map(category =>
         photoService.fetchPhotos(category.id).then(photos =>
-          Object.assign(category, { photos })
-        )
-      ))
+          Object.assign(category, { photos }),
+        ),
+      )),
     ),
   });
 });
@@ -57,36 +62,34 @@ const pages = config.locales.map((locale) => {
 export default () => Promise
   .all(pages)
   .then(langs => langs.reduce((links, lang) => {
-    const
-      rootLinks = [{ url: '', changefreq: 'monthly', priority: 1 }],
-      pageLinks = lang.pages.map(page => ({
-        url: `/${page.alias}`,
+    const rootLinks = [{ url: '', changefreq: 'monthly', priority: 1 }];
+    const pageLinks = lang.pages.map(page => ({
+      url: `/${page.alias}`,
+      changefreq: 'monthly',
+      priority: 1,
+    }));
+    const categoriesLinks = lang.categories.reduce((acc, category) => {
+      const url = category.parent ? `/${category.parent.name}/${category.name}` : `/${category.name}`;
+      const photoLinks = category.photos.map(photo => ({
+        url: `${url}/photo/${photo.id}`,
         changefreq: 'monthly',
-        priority: 1,
-      })),
-      categoriesLinks = lang.categories.reduce((acc, category) => {
-        const
-          url = category.parent ? `/${category.parent.name}/${category.name}` : `/${category.name}`,
-          photoLinks = category.photos.map(photo => ({
-            url: `${url}/photo/${photo.id}`,
-            changefreq: 'monthly',
-            priority: 0.5,
-            img: [config.staticEndpoint, getSrc(photo.src, config.photo.width, config.photo.height)].join('/'),
-          }));
+        priority: 0.5,
+        img: [config.staticEndpoint, getSrc(photo.src, config.photo.width, config.photo.height)].join('/'),
+      }));
 
-        return acc.concat([{
-          url,
-          changefreq: 'monthly',
-          priority: 0.8,
-        }], photoLinks);
-      }, []);
+      return acc.concat([{
+        url,
+        changefreq: 'monthly',
+        priority: 0.8,
+      }], photoLinks);
+    }, []);
 
     return links.concat(
       [].concat(
         rootLinks,
         pageLinks,
         categoriesLinks,
-      ).map(url => Object.assign(url, { langs: [lang.locale] })
+      ).map(url => Object.assign(url, { langs: [lang.locale] }),
     ));
   }, []))
   .then(merge)
--- client/src/lib/utils.js
+++ client/src/lib/utils.js
@@ -4,14 +4,6 @@ import config from '../etc/config';
 export const localeURL = /^(\/)?(ru|en)?($|\/.*$)$/g;
 
 export default {
-  fetchAll(object) {
-    const keys = Object.keys(object);
-
-    return Promise
-      .all(keys.map(k => object[k]))
-      .then(data => keys.reduce((acc, k, idx) => Object.assign(acc, { [k]: data[idx] }), {}));
-  },
-
   getSrc(...args) {
     return [config.staticEndpoint, photo.getSrc(...args)].join('/');
   },
