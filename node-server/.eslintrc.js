767c06b4f8d06ca41220339d95556152e15f82b9
Puchenkin Artem <apuchenkin@gmail.com>
2019-05-21 22:12:55 +0300

    rm node-server

--- node-server/.eslintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    extends: "airbnb-base",
-    env: {
-      node: true
-    },
-    plugins: [
-        "import"
-    ]
-};
--- node-server/scripts/db.sh
+++
@@ -1,1 +0,0 @@
-docker run --name mysql-aws -e MYSQL_ROOT_PASSWORD=r00t -e MYSQL_DATABASE=aws-photo -d -p 3306:3306 mysql
